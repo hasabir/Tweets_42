eb6de4d8ec4559fd87f305cee459fcda9a318afb
hasabir <sabirhajar048@gmail.com>
2024-11-08 19:55:24 +0100

    adding Vectorization class

--- src/Tweets/src/vectorization.py
+++ src/Tweets/src/vectorization.py
@@ -1,12 +1,21 @@
 import pandas as pd
 
 
-def	bag_of_words(data_set):
-    bag_of_words = {}
-    for review in data_set:
-        for word in review.split():
-            if word in bag_of_words:
-                bag_of_words[word] += 1
-            else:
-                bag_of_words[word] = 1
-    return pd.DataFrame.from_dict(bag_of_words, orient='index', columns=['Count']).T
+
+class Vectorization:
+
+    @staticmethod
+    def bag_of_words(data_frame: list) -> dict:
+        bag_of_words = {}
+        for data in data_frame:
+            for word in data:
+                if word in bag_of_words:
+                    bag_of_words[word] += 1
+                else:
+                    bag_of_words[word] = 1
+        return bag_of_words
+    
+    
+    @staticmethod
+    def tf_idf(data_set):
+        pass
