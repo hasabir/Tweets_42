d29c0fd8f928467c4a3356233167f8943b201013
Hajar Sabir <hasabir@e2r13p9.1337.ma>
2024-12-18 13:23:27 +0100

    init

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+# docker pull jupyter/base-notebook
+# docker run -p 8888:8888 -v $(pwd):/home/jovyan/work jupyter/base-notebook
 pip install -r requirements.txt
 python -m spacy download en_core_web_md
 python -m nltk.downloader stopwords
